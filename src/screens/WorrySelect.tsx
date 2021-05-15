0e5b92077718bbd692c872d3c6a3fa7d1f8741f6
kensan914 <52157596+kensan914@users.noreply.github.com>
2021-05-15 23:18:19 +0900

    add ProfileScreen(Template)・TopScreen(Template)

--- src/screens/WorrySelect.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import WorrySelectTemplate from "src/components/templates/WorrySelectTemplate";
-
-const WorrySelect: React.FC = () => {
-  return <WorrySelectTemplate />;
-};
-
-export default WorrySelect;
