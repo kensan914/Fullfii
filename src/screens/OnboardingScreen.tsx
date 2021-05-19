e58892ca1637ed8d5531bb977ba2ee4203bf01ee
林幸嗣 <koji06099@gmail.com>
2021-05-19 12:44:05 +0900

    bottomtabのデザインとintro画面作成

---
+++ src/screens/OnboardingScreen.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { OnboardingTemplate } from "src/components/templates/OnboardingTemplate";
+
+export const OnboardingScreen: React.FC = () => {
+  return <OnboardingTemplate />;
+};
