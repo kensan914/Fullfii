b77e595f06cdad823e556b19507a798847f190c7
kensan914 <52157596+kensan914@users.noreply.github.com>
2021-06-01 21:18:54 +0900

    RoomEditorModalにKeyboardAvoidingView追加

--- src/contexts/DomContext.tsx
+++ src/contexts/DomContext.tsx
@@ -1,27 +1,27 @@
 import React, { createContext, useReducer, useContext } from "react";
 
-import { usePushNotificationParams } from "src/hooks/pushNotifications/usePushNotificationParams";
 import { DomActionType, DomState, DomDispatch } from "src/types/Types.context";
 
 const domReducer = (prevState: DomState, action: DomActionType): DomState => {
   const _taskSchedules = { ...prevState.taskSchedules };
   const _pushNotificationParams = { ...prevState.pushNotificationParams };
+
   switch (action.type) {
-    case "SCHEDULE_TASK":
+    case "SCHEDULE_TASK": {
       /** 指定したtaskKeyのタスクをスケジュール.
        * @param {Object} action [type, taskKey] */
 
       _taskSchedules[action.taskKey] = true;
       return { ...prevState, taskSchedules: _taskSchedules };
-
-    case "DONE_TASK":
+    }
+    case "DONE_TASK": {
       /** スケジュールされたタスクの実行終了後に実行.
        * @param {Object} action [type, taskKey] */
 
       _taskSchedules[action.taskKey] = false;
       return { ...prevState, taskSchedules: _taskSchedules };
-
-    case "SET_PUSH_NOTIFICATION_PARAMS":
+    }
+    case "SET_PUSH_NOTIFICATION_PARAMS": {
       /** set pushNotificationParams.isPermission or pushNotificationParams.isChosenPermission
        * @param {Object} action [type, isPermission, isChosenPermission] */
 
@@ -33,21 +33,21 @@ const domReducer = (prevState: DomState, action: DomActionType): DomState => {
       }
 
       return { ...prevState, pushNotificationParams: _pushNotificationParams };
-
-    case "CONFIGURED_PUSH_NOTIFICATION":
+    }
+    case "CONFIGURED_PUSH_NOTIFICATION": {
       /** push通知設定をした際に実行. pushNotificationParamsを最新状態に更新.
        * @param {Object} action [type] */
 
       _pushNotificationParams.isChanged = true;
       return { ...prevState, pushNotificationParams: _pushNotificationParams };
-
-    case "FINISH_SET_PUSH_NOTIFICATION_PARAMS":
+    }
+    case "FINISH_SET_PUSH_NOTIFICATION_PARAMS": {
       /** pushNotificationParamsを設定をした際に実行.
        * @param {Object} action [type] */
 
       _pushNotificationParams.isChanged = false;
       return { ...prevState, pushNotificationParams: _pushNotificationParams };
-
+    }
     default:
       console.warn(`Not found this action.type.`);
       return { ...prevState };
@@ -79,7 +79,6 @@ export const useDomDispatch = (): DomDispatch => {
 };
 
 export const DomProvider: React.FC = ({ children }) => {
-  usePushNotificationParams();
   const [domState, domDispatch] = useReducer(domReducer, {
     ...initDomState,
     pushNotificationParams: {
