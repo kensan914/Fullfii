f5f259dc580addce97958043ab231add7e106634
林幸嗣 <koji06099@gmail.com>
2021-07-27 08:19:44 +0900

    notificationImageとintroNotificationスクリーン

--- src/components/templates/intro/IntroSignupTemplate/pages/PushNotificationReminderTemplate/index.tsx
+++ src/components/templates/intro/IntroSignupTemplate/pages/PushNotificationReminderTemplate/index.tsx
@@ -14,14 +14,28 @@ import { width, height } from "src/constants";
 import { MAN_AND_WOMAN_IMG, MEN_IMG } from "src/constants/imagePath";
 import { ALERT_MESSAGES } from "src/constants/alertMessages";
 import { NOTIFICATION_IMG } from "src/constants/imagePath";
+import { LECTURE_WOMAN_IMG } from "src/constants/imagePath";
+
 export const PushNotificationReminderTemplate: React.FC = () => {
   return (
     <Block flex style={styles.container}>
-      <Block flex style={styles.notificationImgContainer} >
+      <Block flex={0.3} style={styles.notificationImgContainer} >
         <Image
             style={styles.notificationImg}
             source={ NOTIFICATION_IMG }
           />
+      </Block>
+      <Block flex={0.7} row style={styles.leftMessageContainer}>
+        <Image
+          style={styles.avater}
+          source={ LECTURE_WOMAN_IMG }
+        />
+        <Block>
+          <Block style={styles.leftMessage}>
+            <Text size={14} color={COLORS.BLACK} numberOfLines={2} ellipsizeMode="tail">話し相手のメッセージにすぐ気づけるよ！</Text>
+          </Block>
+          <Text size={12} color={COLORS.LIGHT_GRAY} style={styles.textLineHeight}>21:23</Text>
+        </Block>
       </Block>
     </Block>
   );
@@ -55,7 +69,11 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 16
   },
   notificationImgContainer: {
-    alignItems: "center"
+    alignItems: "center",
+    marginTop: 40
   },
-
+  notificationImg: {
+    height: 147,
+    width: 200
+  }
 })
